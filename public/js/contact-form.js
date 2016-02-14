482442140952f41d20acfbb70cd236ec5b698dbc
William Lacy <williamlacy@uk.ibm.com>
2016-02-14 14:28:44 +0000

    imprive form validation pre submit

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -1,8 +1,8 @@
 var form = '<form id="contactForm" action="/contact" method="post">' +
     '           <div class="col-md-6 contact-left">' +
-    '               <input name="name" type="text" class="text" value="Name *" onfocus="this.value = this.value === \'Name *\' ? \'\' : this.value" onblur="if (this.value == \'\') {this.value = \'Name *\';}">' +
-    '               <input name="email" type="text" class="text" value="Email *" onfocus="this.value = this.value === \'Email *\' ? \'\' : this.value" onblur="if (this.value == \'\') {this.value = \'Email *\';}">' +
-    '               <input name="phone" type="text" class="text" value="Phone *" onfocus="this.value = this.value === \'Phone *\' ? \'\' : this.value" onblur="if (this.value == \'\') {this.value = \'Phone *\';}">' +
+    '               <input name="name" required="required" type="text" class="text" value="Name *" onfocus="this.value = this.value === \'Name *\' ? \'\' : this.value" onblur="if (this.value == \'\') {this.value = \'Name *\';}">' +
+    '               <input name="email" required="required" type="email" class="text" value="Email *" onfocus="this.value = this.value === \'Email *\' ? \'\' : this.value" onblur="if (this.value == \'\') {this.value = \'Email *\';}">' +
+    '               <input name="phone" required="required" type="tel" class="text" value="Phone *" onfocus="this.value = this.value === \'Phone *\' ? \'\' : this.value" onblur="if (this.value == \'\') {this.value = \'Phone *\';}">' +
     '           </div>' +
     '           <div class="col-md-6 contact-right">' +
     '               <textarea name="message" value="Message *" onfocus="this.value = this.value == \'Message *\' ? \'\' : this.value" onblur="if (this.value == \'\') {this.value = \'Message *\';}">Message *</textarea>' +
@@ -12,7 +12,7 @@ var form = '<form id="contactForm" action="/contact" method="post">' +
     '               </button>' +
     '           </div>' +
     '           <div class="clearfix"></div>' +
-    '         </form>';
+    '       </form>';
 var formParent = $(document.currentScript.parentNode);
 $(document).ready(function(){
     formParent.append([form]);
